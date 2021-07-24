2b1cdcff47190dc75e815c6f14f33b88b3ca38a8
crystal1229d <crystal1229d@naver.com>
2021-07-25 02:22:11 +0900

    remove routes.js / add routes/products.js

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('./controller/products.js');
+const productController = require('../controller/products.js');
 
 router.get('/', productController.hello);
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = 5000;
 
 const app = express();
-const productRoutes = require('./routes');
+const productRoutes = require('./routes/products.js');
 
 app.use('/api/products', productRoutes);
 

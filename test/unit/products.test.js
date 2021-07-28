a8d6680c46d25a940f66014a76e5c29d50255628
crystal1229d <crystal1229d@naver.com>
2021-07-28 22:51:19 +0900

    add some mock to Product Controller Create test

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,17 +1,32 @@
 const productController = require('../../controller/products');
 const productModel = require('../../models/Product');
+const httpMocks = require('node-mocks-http');
+const newProduct = require('../data/new-product.json');
 
 productModel.create = jest.fn(); // Product model 파일에 의존하지 않기 위해 사용
 
+let req, res, next;
+
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
+
 describe('Product Controller Create', () => { // 테스트 그룹
-    // it() or test() : 테스트 케이스 
+
+    beforeEach(() => {
+        req.body = newProduct;
+    })
     
+    // it() or test() : 테스트 케이스 
     it('should have a createProduct function', () => {
         expect(typeof productController.createProduct).toBe('function');
     })
 
     it('should call ProductModel.create', () => {
-        productController.createProduct();
-        expect(productModel.create).toBeCalled();
+        productController.createProduct(req, res, next);
+        expect(productModel.create).toBeCalledWith(newProduct);
     })
+
 })

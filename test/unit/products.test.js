fad363963753f8346f225909d44764693c12f47e
crystal1229d <crystal1229d@naver.com>
2021-07-29 20:50:23 +0900

    add completion to Product Controller Create unit test

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -3,7 +3,7 @@ const productModel = require('../../models/Product');
 const httpMocks = require('node-mocks-http');
 const newProduct = require('../data/new-product.json');
 
-productModel.create = jest.fn(); // Product model 파일에 의존하지 않기 위해 사용
+productModel.create = jest.fn(); // Product model 파일에 의존하지 않기 위해 사용. productModel.create 함수가 잘 작동할거라는 가정하에 mock function으로 생성
 
 let req, res, next;
 
@@ -20,13 +20,22 @@ describe('Product Controller Create', () => { // 테스트 그룹
     })
     
     // it() or test() : 테스트 케이스 
-    it('should have a createProduct function', () => {
+    it ('should have a createProduct function', () => {
         expect(typeof productController.createProduct).toBe('function');
     })
-
-    it('should call ProductModel.create', () => {
+    it ('should call ProductModel.create', () => {
         productController.createProduct(req, res, next);
         expect(productModel.create).toBeCalledWith(newProduct);
+    })
+    it ('should return 201 response code', () => {
+        productController.createProduct(req, res, next);
+        expect(res.statusCode).toBe(201);
+        expect(res._isEndCalled()).toBeTruthy(); // _isEndCalled : 결과값이 잘 전송됐는지 확인
+    })
+    it ('should return json body in response', () => {
+        productModel.create.mockReturnValue(newProduct);
+        productController.createProduct(req, res, next);
+        expect(res._getJSONData()).toStrictEqual(newProduct);
     })
 
 })

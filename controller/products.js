15e0a08a426a21041ca3d8fe6605e517eb46d7cf
crystal1229d <crystal1229d@naver.com>
2021-08-01 23:09:07 +0900

    add body-parser for req data

--- controller/products.js
+++ controller/products.js
@@ -1,6 +1,8 @@
 const productModel = require('../models/Product');
 
-exports.createProduct = (req, res, next) => { 
-    const createdProduct = productModel.create(req.body);
+exports.createProduct = async (req, res, next) => { 
+    console.log(req.body);
+    const createdProduct = await productModel.create(req.body);
+    console.log(createdProduct);
     res.status(201).json(createdProduct);
 };
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
+const bodyParser = require('body-parser'); 
 const PORT = 5000;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false})); 
+app.use(bodyParser.json());
+
 const productRoutes = require('./routes/products.js');
 
 const mongoose = require('mongoose');
@@ -13,7 +17,8 @@ mongoose.connect(
         useNewUrlParser: true,
         useUnifiedTopology: true  
     }
-).then(() => console.log('MongoDB connected...'))
+)
+.then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
 
 app.use('/api/products', productRoutes);
